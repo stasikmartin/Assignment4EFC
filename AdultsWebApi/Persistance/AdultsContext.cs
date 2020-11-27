bb402dde55cd6c3bde3ead783240b696ea999894
stasikmartin <58514490+stasikmartin@users.noreply.github.com>
2020-11-27 15:13:38 +0100

    Add files via upload

---
+++ AdultsWebApi/Persistance/AdultsContext.cs
@@ -0,0 +1,18 @@
+﻿using AdultsWebApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdultsWebApi.Persistance
+{
+    public class AdultsContext : DbContext
+    {
+        
+        
+        public DbSet<Adult> adults { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            // name of database
+            optionsBuilder.UseSqlite("Data Source = Adults.db");
+        }
+        
+    }
+}
---
+++ AdultsWebApi/Persistance/UsersContext.cs
@@ -0,0 +1,18 @@
+﻿using AdultsWebApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdultsWebApi.Persistance
+{
+    public class UsersContext : DbContext
+    {
+        
+        public DbSet<User> users { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            // name of database
+            optionsBuilder.UseSqlite("Data Source = Users.db");
+        }
+        
+        
+    }
+}
